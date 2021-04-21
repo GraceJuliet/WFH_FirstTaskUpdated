d0b519d6e3a31b8cc35b46f2a987005db426bac3
GraceJuliet <71426771+GraceJuliet@users.noreply.github.com>
2021-04-21 12:58:32 +0530

    Add files via upload

---
+++ FirstTaskUpdated/FirstTaskUpdated/Model/Command.cs
@@ -0,0 +1,37 @@
+﻿using FirstTaskUpdated;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FirstTaskUpdated
+{
+    class Command : ICommand
+
+    {
+        private ViewModel buttonviewmodel;
+
+
+        public Command(ViewModel vml)
+        {
+            this.buttonviewmodel = vml;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            buttonviewmodel.executeMethod();
+
+            //buttonviewmodel.Name = "Frequent Modifications";
+            buttonviewmodel.OnPropertyChanged(buttonviewmodel.Name);
+        }
+    }
+}
---
+++ FirstTaskUpdated/FirstTaskUpdated/ViewModel/ViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FirstTaskUpdated
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public Command cmd { get; set; }
+
+        public ViewModel()
+        {
+            cmd = new Command(this);
+
+        }
+
+        private string username;
+        private string password;
+       
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+
+       
+
+        public void executeMethod()
+        {
+            if (Name == "grace")
+            {
+                MessageBox.Show("Login Successful");
+            }
+            else
+                MessageBox.Show("Invalid Credentials");
+        }
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+    }
+}
